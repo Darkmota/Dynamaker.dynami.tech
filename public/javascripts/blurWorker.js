627ea67a905b678b18ace2b1914f79de9189f9d6
jmakxd <80338394+jmakxd@users.noreply.github.com>
2021-04-07 20:19:23 +0800

    Delete public directory

--- public/javascripts/blurWorker.js
+++
@@ -1,9 +0,0 @@
-importScripts("StackBlur.js");
-
-var ans = 0,
-	i = 0;
-this.onmessage = function(event) {
-	var imageData = event.data;
-	stackBlurImageData(imageData, 0, 0, 1920, 1080, 10);
-	postMessage(imageData);
-}
